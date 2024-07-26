cd33a09361dffd2ea90586df5701bc4a39b2216b
The Byte Squad <adityaparmar9900@gmail.com>
2024-07-26 11:05:41 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const skills = document.querySelectorAll('.skill');
 
+  skills.forEach(skill => {
+    skill.addEventListener('mouseover', () => {
+            skill.style.transform = 'scale(1.1)';
+    });
+
+    skill.addEventListener('mouseout', () => {
+      skill.style.transform = 'scale(1)';
+    });
+  });
+});
